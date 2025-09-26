4e1e668196ae51ce169397f0eae56b818401bc88
Matt Catalano <mattcat26@gmail.com>
2025-09-26 17:37:43 -0500

    set up currently spanking and backfill book history

--- packages/aws-nextjs/lib/service/BookService.ts
+++ packages/aws-nextjs/lib/service/BookService.ts
@@ -1,8 +1,10 @@
-import { Book } from "@/model/Book";
+import { Book } from "@/model/book";
 
 export interface IBookService {
 
     getCompleted(limit: number, offset: number): Promise<Book[]>;
 
-    getUserBooks(userId: number, limit: number, offset: number): Promise<Book[]>
+    getUserBooks(userId: number, limit: number, offset: number): Promise<Book[]>;
+
+    getCurrentBook(): Promise<Book | null>;
 }
