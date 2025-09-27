3fcf0aad4534723147727c5be496030cbb40d321
Matt Catalano <mattcat26@gmail.com>
2025-09-27 15:38:03 -0500

    repeatable

--- packages/backend/src/main/resources/db/migration/R__seed.sql
+++ packages/backend/src/main/resources/db/migration/R__seed.sql
@@ -1,4 +1,10 @@
--- Insert "The Road" by Cormac McCarthy with start date September 18th 2025
+INSERT INTO users (username, email)
+VALUES ('lsimanonok', 'leo@simanonok.com'),
+        ('mcatalano', 'mattcat26@gmail.com'),
+        ('sminix', 'supersam5699@gmail.com'),
+        ('qurell', 'quinn@urell.com'),
+        ('gharris', 'gabe@harris.com')
+ON CONFLICT (username, email) DO NOTHING;
 
 INSERT INTO books (title, author, started, finished, selected_by)
 VALUES ('The Road', 'Cormac McCarthy', '2025-09-18', NULL, 'gharris'),
