b9c6c39c03a0257261e60424976766d7ed9d9a2d
Leo <lsimanonok@tulane.edu>
2025-10-15 17:30:50 -0500

    updated book model

--- packages/aws-nextjs/lib/model/book.ts
+++ packages/aws-nextjs/lib/model/book.ts
@@ -3,23 +3,17 @@ export class Book {
     public readonly title: string;
     public readonly author: string;
     public readonly cover_id: number | null;
-    public readonly started?: Date;
-    public readonly finished?: Date;
 
     private constructor(
         id: number,
         title: string,
         author: string,
         cover_id: number | null,
-        started?: Date,
-        finished?: Date
     ) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.cover_id = cover_id,
-            this.started = started;
-        this.finished = finished;
     }
 
     public static fromJSON(json: any): Book {
@@ -31,16 +25,11 @@ export class Book {
             throw new Error("Invalid json for book creation...");
         }
 
-        const started = json.started ? new Date(json.started) : undefined;
-        const finished = json.finished ? new Date(json.finished) : undefined;
-
         return new Book(
             json.id,
             json.title,
             json.author,
             json.cover_id,
-            started,
-            finished
         );
     }
 }
