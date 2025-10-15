856a4ed18e5cc19e2d2516900531703471af1b8f
Leo <lsimanonok@tulane.edu>
2025-10-15 15:31:10 -0500

    separated books from users, created reading lists for holding user data, club history for tracking books read by club, renames reviews and made them dependent on club history

--- packages/backend/src/main/resources/db/migration/V1.1__books.sql
+++ packages/backend/src/main/resources/db/migration/V1.1__books.sql
@@ -6,9 +6,6 @@ CREATE TABLE books (
     author VARCHAR(255) NOT NULL, 
     cover_id INTEGER NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    started DATE,
-    finished DATE,
-    selected_by INT NOT NULL REFERENCES users(id),
     UNIQUE(title, author)
 );
 
---
+++ packages/backend/src/main/resources/db/migration/V1.3__club_history.sql
@@ -0,0 +1,9 @@
+CREATE TABLE club_history (
+    id SERIAL PRIMARY KEY,
+    selected_by INT NOT NULL REFERENCES users(id),
+    book_id INT NOT NULL REFERENCES books(id),
+    started DATE,
+    finished DATE
+);
+
+CREATE UNIQUE INDEX enforce_single_null_in_finished ON club_history ((finished IS NULL)) WHERE finished IS NULL;
