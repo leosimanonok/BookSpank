be0fdc6525c2ffdaadb7a977bd8f6c1ed79dbf67
Leo <lsimanonok@tulane.edu>
2025-10-15 15:59:16 -0500

    updated book model

--- packages/backend/src/main/java/com/bookspank/backend/model/Book.java
+++ packages/backend/src/main/java/com/bookspank/backend/model/Book.java
@@ -1,64 +1,16 @@
 package com.bookspank.backend.model;
 
-import java.time.LocalDate;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     private Integer id;
     private String title;
     private String author;
     private Integer cover_id;
-    private LocalDate started;
-    private LocalDate finished;
-    private Integer selected_by;
 
-    public Book(
-            Integer id,
-            String title,
-            String author,
-            Integer cover_id,
-            Integer selected_by) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.cover_id = cover_id;
-        this.selected_by = selected_by;
-    }
-
-    public Book(
-            Integer id,
-            String title,
-            String author,
-            Integer cover_id,
-            Integer selected_by,
-            LocalDate started) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.cover_id = cover_id;
-        this.selected_by = selected_by;
-        this.started = started;
-    }
-
-    public Book(
-            Integer id,
-            String title,
-            String author,
-            Integer cover_id,
-            Integer selected_by,
-            LocalDate started,
-            LocalDate finished) {
-
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.cover_id = cover_id;
-        this.selected_by = selected_by;
-        this.started = started;
-        this.finished = finished;
-    }
 }
--- packages/backend/src/main/resources/db/migration/V1.1__books.sql
+++ packages/backend/src/main/resources/db/migration/V1.1__books.sql
@@ -1,12 +1,9 @@
---- Create books table 
-
-CREATE TABLE books (
-    id SERIAL PRIMARY KEY, 
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL, 
-    cover_id INTEGER NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    UNIQUE(title, author)
-);
-
-CREATE UNIQUE INDEX enforce_single_null_in_finished ON books ((finished IS NULL)) WHERE finished IS NULL;
+CREATE TABLE
+    books (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        author VARCHAR(255) NOT NULL,
+        cover_id INTEGER NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW (),
+        UNIQUE (title, author)
+    );
