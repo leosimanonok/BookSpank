2f9e237912cae020bdbb12e65ac0a4cc461a6a30
Leo <lsimanonok@tulane.edu>
2025-10-19 12:33:27 -0500

    removed position, added want_to_read_next to reading list entries table

--- packages/backend/src/main/resources/db/migration/V1.2__reading_lists.sql
+++ packages/backend/src/main/resources/db/migration/V1.2__reading_lists.sql
@@ -2,6 +2,6 @@ CREATE TABLE
     reading_list_entries (
         user_id INT NOT NULL REFERENCES users (id),
         book_id INT NOT NULL REFERENCES books (id),
-        position INT NOT NULL,
+        want_to_read_next BOOLEAN NOT NULL DEFAULT FALSE,
         PRIMARY KEY (user_id, book_id)
     );
